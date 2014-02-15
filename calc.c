ce4eacc2b35826c67af9f6adcc634a6313f8c7a1
abhivij <abhivij.game@gmail.com>
2014-02-15 12:31:43 +0530

    Added the functions, made the code pretty

--- calc.c
+++ calc.c
@@ -1,23 +1,48 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int add(int x, int y) ;
+int multiply(int x, int y) ;
+int divide(int x, int y) ;
+int subtract(int x, int y) ;
+
 int main(){
 	int x,y;
 	char ch;
 	scanf("%d %c %d",&x,&c,&y);
-	switch(c){
-		case('+'):
-			z=add(x,y);
-			break;
-		case('*'):
-			z=multiply(x,y);
-			break;
-		case('/'):
-			z=divide(x,y);
-			break;
-		case('-'):
-			z=subtract(x,y);
-			break;
+	switch(c)
+	{
+		case('+'):	z=add(x,y);
+				break;
+
+		case('-'):	z=subtract(x,y);
+				break;
+
+		case('*'):	z=multiply(x,y);
+				break;
+
+		case('/'):	z=divide(x,y);
+				break;
 	}
-	printf("result is %d\n",z);
+	printf("RESULT = %d\n",z);
+}
+
+int add(int x, int y)
+{
+	return x + y ;
+}
+
+int subtract(int x, int y)
+{
+	return x - y ;
+}
+
+int multiply(int x, int y)
+{
+	return x * y ;
+}
+
+int divide(int x, int y)
+{
+	return x / y ;
 }
